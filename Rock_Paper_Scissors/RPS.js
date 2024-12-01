2c37db17de70dd45765ca00c84e8a90c7f560acf
Chien <chieniscool123@gmail.com>
2024-11-30 22:21:55 -0800

    update rps game

--- Rock_Paper_Scissors/RPS.js
+++ Rock_Paper_Scissors/RPS.js
@@ -1,6 +1,35 @@
 function getComputerChoice(){
     let num = Math.floor( Math.random() * 2) ;
-    return num;
+    if ( num == 0) {
+        num = "Scissor";
+    }
+    else if (num==1) {
+       num = "Rock";
+    }
+    else if (num ==2) {
+        num = "Paper";
+    }
+    return console.log("The MACHINE chose " + num);
+
 };
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let num = prompt("Choose 1,2 or 3 to play \n 1. Scissor \n 2.Rock \n 3.Paper ");
+    if ( num == 1) {
+        num = "Scissor";
+    }
+    else if (num==2) {
+       num = "Rock";
+    }
+    else if (num ==3) {
+        num = "Paper";
+    }
+    else {
+        console.log("Please select 1-3");
+        getHumanChoice();
+    }
+    return console.log("I choose " + num);
+}
+
+getHumanChoice();
+getComputerChoice();
