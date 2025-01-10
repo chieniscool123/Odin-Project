053aeb58c641d037fbd85cb76f6398d5b940a840
Chien <chieniscool123@gmail.com>
2025-01-10 12:55:15 -0800

    80 percent done with calculator

--- calculator/cal.js
+++ calculator/cal.js
@@ -7,75 +7,126 @@ const screen = document.getElementById("screen");
 
 
 document.getElementById("=").addEventListener("click", () => {
-    screen.textContent = operate(str);
-});
-
-
-document.getElementById("7").addEventListener("click", () => {
-    screen.textContent = "7"
-    str += "7"
+    screen.textContent = (operate(str));
 
 });
 
 
 document.getElementById("+").addEventListener("click", () => {
-    screen.textContent = "+"
+    screen.textContent += "+"
     str += "+"
 
 });
 
+
+document.getElementById("/").addEventListener("click", () => {
+    screen.textContent += "/"
+    str += "/"
+
+});
+
+
+document.getElementById("-").addEventListener("click", () => {
+    screen.textContent += "-"
+    str += "-"
+
+});
+
+
+document.getElementById("*").addEventListener("click", () => {
+    screen.textContent += "*"
+    str += "*"
+
+});
+
+document.getElementById("1").addEventListener("click", () => {
+    screen.textContent += "1"
+    str += "1"
+});
+
+document.getElementById("2").addEventListener("click", () => {
+    screen.textContent += "2"
+    str += "2"
+});
+
+document.getElementById("3").addEventListener("click", () => {
+    screen.textContent += "3"
+    str += "3"
+});
+
+document.getElementById("4").addEventListener("click", () => {
+    screen.textContent += "4"
+    str += "4"
+});
+
+document.getElementById("5").addEventListener("click", () => {
+    screen.textContent += "5"
+    str += "5"
+});
+
+
+document.getElementById("6").addEventListener("click", () => {
+    screen.textContent += "6"
+    str += "6"
+});
+
+
+document.getElementById("7").addEventListener("click", () => {
+    screen.textContent += "7"
+    str += "7"
+});
+
 document.getElementById("8").addEventListener("click", () => {
-    screen.textContent = "8"
+    screen.textContent += "8"
     str += "8"
 });
 
 
+document.getElementById("9").addEventListener("click", () => {
+    screen.textContent += "9"
+    str += "9"
+});
 
 
+document.getElementById("clear").addEventListener("click", () => {
+    screen.textContent = ""
+    str = ""
+});
+
+document.getElementById("delete").addEventListener("click", () => {
+    screen.textContent = screen.textContent
+        .toString()
+        .slice(0, -1);
+
+    str = screen.textContent;
+});
 
 
-function add(total, current_value) {
-    return total + current_value;
-    current_value = 0;
-    return total;
-}
-function subtract(total, current_value) {
-    return total - current_value;
-    current_value = 0;
-    return total;
-}
-
-function multiply(total, current_value) {
-    return total * current_value;
-    current_value = 0;
-    return total;
-}
-
-function divide(total, current_value) {
-    return total / current_value;
-    current_value = 0;
-    return total;
-
-}
 
 function operate(str) {
-    let number = str.split(/[+\-/*]+/);
 
-    const operator = str.split(/[0-9]+/);
+    // \d is for one or more digit from 0-9
+    // regex = regular expression
+    // match[0] will be equal to the orignal string so we must start at match 1
+    const regex = /(\d+)([+\-*/])(\d+)/;
+    const match = str.match(regex);
 
+    const num1 = parseFloat(match[1]);
+    const operator = match[2];
+    const num2 = parseFloat(match[3]);
 
     switch (operator) {
         case '+':
-            return number[0] + number[1]
+            return num1 + num2
             break;
         case '-':
-            return number[0] - number[1];
+            return num1 - num2
             break;
         case '*':
-            return number[0] * number[1];
+            return num1 * num2
             break;
         case '/':
-            return number[0] / number[1];
+            return num1 / num2
             break;
     }
 
