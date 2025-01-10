f58bcceae9a718b53a712ec6668d10f20870fe6a
Chien <chieniscool123@gmail.com>
2025-01-09 23:39:06 -0800

    finished with ui

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,6 +1,39 @@
 let total = 0;
 let current_value = 0;
 
+let str = ""
+
+const screen = document.getElementById("screen");
+
+
+document.getElementById("=").addEventListener("click", () => {
+    screen.textContent = operate(str);
+});
+
+
+document.getElementById("7").addEventListener("click", () => {
+    screen.textContent = "7"
+    str += "7"
+
+});
+
+
+document.getElementById("+").addEventListener("click", () => {
+    screen.textContent = "+"
+    str += "+"
+
+});
+
+document.getElementById("8").addEventListener("click", () => {
+    screen.textContent = "8"
+    str += "8"
+});
+
+
+
+
+
+
 function add(total, current_value) {
     return total + current_value;
     current_value = 0;
@@ -25,25 +58,28 @@ function divide(total, current_value) {
 
 }
 
-function operate(num_1, num_2, operator) {
+function operate(str) {
+    let number = str.split(/[+\-/*]+/);
+
+    const operator = str.split(/[0-9]+/);
+
+
     switch (operator) {
         case '+':
-            add(num_1, num2)
+            return number[0] + number[1]
             break;
-
         case '-':
-            subtract(num_1, num2)
+            return number[0] - number[1];
             break;
-
         case '*':
-            multiply(num_1, num2)
+            return number[0] * number[1];
             break;
-
         case '/':
-            divide(num_1, num2)
+            return number[0] / number[1];
             break;
-
     }
+
 }
+
 
 
